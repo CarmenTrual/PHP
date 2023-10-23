56b317b698d6b2d69d5c2453b20ef355acfd587e
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-10-23 17:52:41 +0200

    PHP

---
+++ Relación_3.4_Funciones/Ej7/Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prueba de funciones</title>
+</head>
+<body>
+<?php 
+//Crea una biblioteca de funciones para arrays bidimensionales (de dos dimensiones) de números enteros que contenga las siguientes funciones:
+
+
+  ?>
---
+++ Relación_3.4_Funciones/Ej7/funciones.php
@@ -0,0 +1,9 @@
+<?php
+//FUNCIONES
+
+// 1. generaArrayBiInt: Genera un array de tamaño n x m con números aleatorios cuyo intervalo (mínimo y máximo) se indica como parámetro.
+
+// 2. filaDeArrayBiInt: Devuelve la fila i-ésima del array que se pasa como parámetro.
+
+// 3. columnaDeArrayBiInt: Devuelve la columna j-ésima del array que se pasa como parámetro.
+?>
