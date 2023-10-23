4a050aa5b3338274f9e90f56d8f2c2906fb461b8
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-10-23 17:23:29 +0200

    PHP

--- Relación_3.4_Funciones/Ej6/Funciones.php
+++ Relación_3.4_Funciones/Ej6/Funciones.php
@@ -36,11 +36,39 @@ function mediaArrayInt($array){
 }
 
 // 5. estaEnArrayInt: Dice si un número está o no dentro de un array
-function estaEnArrayInt(){
-  
+function estaEnArrayInt($array, $numero){
+  return in_array($numero, $array); //in_array busca un valor dentro del array y devuelve true si está y false si no está.
 }
 
+// 6. posicionEnArray: Busca un número en un array y devuelve la posición (el índice) en la que se encuentra.
+function posicionEnArray($array, $numero,){
+  $posicion = array_search($numero, $array); //array_search busca un valor y devuelve la PRIMERA posición en la que se encuentra.
+
+  if ($posicion == false){
+    return "El número " . $numero . " no está en el array.";
+  } else {
+    return "El número " . $numero . " está en la posición número " . $posicion;
+  }
+}
+
+// 7. volteaArrayInt: Le da la vuelta a un array
+function volteaArrayInt($array){
+  return array_reverse($array);//array_reverse voltea un array (de 1, 2, 3, 4 a 4, 3, 2, 1).
+}
+
+// 8. rotaDerechaArrayInt: Rota n posiciones a la derecha los números de un array.
+function rotaDerechaArrayInt($array, $n){
+  for($i = 0; $i < $n; $i++){
+    array_unshift($array, array_pop($array)); //array_pop quita el último elemento del array y array_unshift lo mete al principio.
+  }
+  return $array;
+}
+
+// 9. rotaIzquierdaArrayInt: Rota n posiciones a la izquierda los números de un array.
+function rotaIzquierdaArrayInt($array, $n){
+  for ($i = 0; $i < $n; $i++){
+    array_push($array, array_shift($array));//array_shift quita el primer elemento del array y array_push lo pone en el final.
+  }
+  return $array;
+}
 ?>
-
-
-
--- Relación_3.4_Funciones/Ej6/Index.php
+++ Relación_3.4_Funciones/Ej6/Index.php
@@ -46,8 +46,62 @@ $media = mediaArrayInt($array);
 echo "Array: " . implode(", ", $array) . "<br>";
 echo "<p>La media del array es: " . $media . ".</p>" ;
 
+//Función 5.
+echo "<h3>estaEnArray</h3>";
+
+$array = generaArrayInt(5, 1, 20);
+$numero = 12;//probamos con el 12 por ejemplo.
+$estaEnArray = estaEnArrayInt($array, $numero);
+
+echo "Array: " . implode(", ", $array) . "</br>";
+if($estaEnArray){
+  echo "<p>El número " . $numero . " si está en el array.". "</p>";
+}else{
+  echo "<p>El número " . $numero . " no está en el array.". "</p>";
+}
+
+//Función 6.
+echo "<h3>posicionEnArray</h3>";
+
+$array = generaArrayInt(5, 1, 20);
+$numero = 12;
+$posicion = posicionEnArray($array, $numero);
+
+echo "Array: " . implode(", ", $array) . "</br>";
+echo "<p>" . $posicion . "</p>";
+
+//Función 7.
+echo "<h3>volteaArrayInt</h3>";
+
+$array = generaArrayInt(5, 1, 20);
+$arrayVolteado = volteaArrayInt($array);
+
+echo "Array: " . implode(", ", $array) . " => " . implode(", ", $arrayVolteado) . "<br>";
+
+//Función 8.
+echo "<h3>rotaDerechaArrayInt</h3>";
+
+$array = generaArrayInt(5, 1, 20);
+$n = 2; // probamos con 2 rotaciones por ejemplo.
+$arrayRotado = rotaDerechaArrayInt($array, $n);
+
+echo "Array: " . implode(", ", $array) . " => " . implode(", ", $arrayRotado) . "<br>";
+echo "Hemos rotado " . $n . " posiciones a la derecha el array: " ;
+
+//Función 9.
+echo "<h3>rotaIzquierdaArrayInt</h3>";
+
+$array = generaArrayInt(5, 1, 20);
+$n = 2;
+$arrayRotado = rotaIzquierdaArrayInt($array, $n);
+
+echo "Array: " . implode(", ", $array) . " => " . implode(", ", $arrayRotado) . "<br>";
+echo "Hemos rotado " . $n . " posiciones a la izquierda el array: " ;
+
+
+
   ?>
   </body>
   </html>
-  
+
 
